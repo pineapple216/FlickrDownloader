90f0e433b8a974861c259db8db2e78f2d2ebda5c
Koen Hendriks <koenhendriks89@gmail.com>
2015-06-23 19:14:14 +0200

    Initial Commit

---
+++ FlickrDownloader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlickrDownloader
+//
+//  Created by Koen Hendriks on 23/06/15.
+//  Copyright (c) 2015 Koen Hendriks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FlickrDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrDownloader
+//
+//  Created by Koen Hendriks on 23/06/15.
+//  Copyright (c) 2015 Koen Hendriks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
