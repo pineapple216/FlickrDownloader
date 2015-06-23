8dfd46b7190dc5ea88742a4102e502fef0758bb9
Koen Hendriks <koenhendriks89@gmail.com>
2015-06-23 20:25:45 +0200

    Initial commit, storyboard setup

---
+++ FlickrDownloader/PlacesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesTableViewController.h
+//  FlickrDownloader
+//
+//  Created by Koen Hendriks on 23/06/15.
+//  Copyright (c) 2015 Koen Hendriks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesTableViewController : UITableViewController
+
+@end
